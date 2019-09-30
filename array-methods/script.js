a8da73db6994a9929aa41019d0f801e33008d157
suraj_122 <122suraj0@gmail.com>
2019-09-30 14:53:05 +0530

    complete task on array methodS

--- array-methods/script.js
+++ array-methods/script.js
@@ -1,108 +1,117 @@
 "use strict";
 
-// const items = [
-//   { name: "Bike", price: 1000 },
-//   { name: "TV", price: 200 },
-//   { name: "Album", price: 300 },
-//   { name: "Book", price: 10 },
-//   { name: "Phone", price: 500 },
-//   { name: "computer", price: 1000 },
-//   { name: "keyboard", price: 30 }
-// ];
-// // Filter
-// const filterItems = items.filter(item => {
-//   return item.price <= 500;
-// });
-// console.log(filterItems);
+const items = [
+  { name: "Bike", price: 1000 },
+  { name: "TV", price: 200 },
+  { name: "Album", price: 300 },
+  { name: "Book", price: 10 },
+  { name: "Phone", price: 500 },
+  { name: "computer", price: 1000 },
+  { name: "keyboard", price: 30 }
+];
+// Filter
+const filterItems = items.filter(item => {
+  return item.price <= 500;
+});
+console.log(filterItems);
 
-// // Map
+// Map
 
-// const itemNames = items.map(item => {
-//   return item.price;
-// });
+const itemNames = items.map(item => {
+  return item.price;
+});
 
-// console.log(itemNames);
+console.log(itemNames);
 
-// // Find
+// Find
 
-// const foundItem = items.find(item => {
-//   return item.name === "Bike";
-// });
-// console.log(foundItem);
+const foundItem = items.find(item => {
+  return item.name === "Bike";
+});
+console.log(foundItem);
 
-// // Foreach
+// Foreach
 
-// items.forEach(item => {
-//   console.log(item.name);
-// });
+items.forEach(item => {
+  console.log(item.name);
+});
 
-// // Some
+// Some
 
-// const expensiveItems = items.some(item => {
-//   return item.price <= 1000;
-// });
+const expensiveItems = items.some(item => {
+  return item.price <= 1000;
+});
 
-// console.log(expensiveItems);
+console.log(expensiveItems);
 
-// // Every
+// Every
 
-// const inExpensiveItems = items.every(item => {
-//   return item.price <= 1000;
-// });
-// console.log(inExpensiveItems);
+const inExpensiveItems = items.every(item => {
+  return item.price <= 1000;
+});
+console.log(inExpensiveItems);
 
-// // Reduce
+// Reduce
 
-// let total = items.reduce((current, item) => {
-//   return current + item.price;
-// }, 0);
+let total = items.reduce((current, item) => {
+  return current + item.price;
+}, 0);
 
-// console.log(total);
+console.log(total);
 
-// // Tasks
+// Tasks
 
-// // Translate border-left-width to borderLeftWidth
+// Translate border-left-width to borderLeftWidth
 
-// function camelize(str) {
-//   return str
-//     .split("-")
-//     .map((item, index) =>
-//       index === 0 ? item : item[0].toUpperCase() + item.slice(1)
-//     )
-//     .join("");
-// }
-// console.log(camelize("-webkit-transition"));
+function camelize(str) {
+  return str
+    .split("-")
+    .map((item, index) =>
+      index === 0 ? item : item[0].toUpperCase() + item.slice(1)
+    )
+    .join("");
+}
+console.log(camelize("-webkit-transition"));
 
-// // Filter range
-// function filterRange(arr, a, b) {
-//   return arr.filter(item => {
-//     return item >= a && item <= b;
-//   });
-// }
-let arr = [5, 2, 1, -10, 8];
-// console.log(filterRange(arr, 1, 5));
+// Filter range
+function filterRange(arr, a, b) {
+  return arr.filter(item => {
+    return item >= a && item <= b;
+  });
+}
+let arr = [5, 3, 8, 1];
+console.log(filterRange(arr, 1, 5));
 
-// // Filter range "in place"
+// Filter range "in place"
 
-// function filterRangeInPlace(arr, a, b) {}
-// console.log(filterRangeInPlace(arr, 1, 4));
+function filterRangeInPlace(arr, a, b) {
+  for (let i = 0; i < arr.length; i++) {
+    let val = arr[i];
+    if (val < a || val > b) {
+      arr.splice(i, 1);
+      i--;
+    }
+  }
+}
+filterRangeInPlace(arr, 1, 4);
+console.log(arr);
 
-// // Sort in the reverse order
-// function sortArr(arr) {
-//   return arr.sort((a, b) => b - a);
-// }
-// console.log(sortArr(arr));
+// Sort in the reverse order
+function sortArr(arr) {
+  return arr.sort((a, b) => b - a);
+}
+console.log(sortArr(arr));
 
-// // Copy and sort array
-// let arr1 = ["HTML", "JavaScript", "CSS"];
+// Copy and sort array
+let arr1 = ["HTML", "JavaScript", "CSS"];
 
-// function copySorted(arr) {
-//   return arr1.slice().sort();
-// }
+function copySorted(arr) {
+  return arr1.slice().sort();
+}
 
-// let sorted = copySorted(arr1);
-// console.log(sorted);
-// console.log(arr1);
+let sorted = copySorted(arr1);
+console.log(sorted);
+console.log(arr1);
 
 // Create an extendable calculator
 
@@ -131,3 +140,61 @@ powerCalc.addMethod("*", (a, b) => a * b);
 powerCalc.addMethod("/", (a, b) => a / b);
 powerCalc.addMethod("**", (a, b) => a ** b);
 console.log(powerCalc.calculate("3 * 7"));
+
+// Map to names
+let john = { name: "John", surname: "Smith", id: 1, age: 25 };
+let pete = { name: "Pete", surname: "Hunt", id: 2, age: 30 };
+let mary = { name: "Mary", surname: "Key", id: 3, age: 28 };
+
+let users = [john, pete, mary];
+let names = users.map(item => item.name);
+console.log(names);
+
+// Map to objects
+
+let usersMapped = users.map(item => ({
+  fullName: `${item.name} ${item.surname}`,
+  id: item.id
+}));
+
+console.log(usersMapped);
+console.log(usersMapped[0].id);
+console.log(usersMapped[0].fullName);
+
+// Shuffle an array
+
+// Get average age
+
+function getAverageAge(users) {
+  return (
+    users.reduce((current, item) => {
+      return current + item.age;
+    }, 0) / users.length
+  );
+}
+console.log(getAverageAge(users));
+
+// Filter unique array members
+function unique(arr) {
+  let result = [];
+  for (let str of arr) {
+    if (!result.includes(str)) {
+      result.push(str);
+    }
+  }
+  return result;
+}
+
+let strings = [
+  "Hare",
+  "Krishna",
+  "Hare",
+  "Krishna",
+  "Krishna",
+  "Krishna",
+  "Hare",
+  "Hare",
+  ":-O"
+];
+
+console.log(unique(strings));
